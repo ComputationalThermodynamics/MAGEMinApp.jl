c5ee6f44d5e3d41714a7fa6943e1e73864200296
nicolasriel <a.nicolas.riel@gmail.com>
2023-10-02 17:09:26 +0200

    added mre fields

--- Tab_PhaseDiagram.jl
+++ Tab_PhaseDiagram.jl
@@ -14,17 +14,25 @@ function Tab_PhaseDiagram()
                                 dbc_row([
                                     dbc_col([ 
                                         html_h1("Field", style = Dict("textAlign" => "center","font-size" => "120%", "marginTop" => 8)),
-                                    ]),
+                                    ], width=3),
                                     dbc_col([
                                         dcc_dropdown(   id      = "fields-dropdown",
                                                         options = [
                                                             (label = "Variance",                value = "nvar"),
                                                             (label = "Number of stable phases", value = "nsp"),
+                                                            (label = "G system",                value = "G_system"),
+                                                            (label = "Entropy",                 value = "entropy"),
+                                                            (label = "Enthalpy",                value = "enthalpy"),
+                                                            (label = "Oxygen fugacity",         value = "fO2"),
                                                             (label = "ρ_system",                value = "rho"),
                                                             (label = "ρ_solid",                 value = "rho_S"),
                                                             (label = "ρ_melt",                  value = "rho_M"),
+                                                            (label = "Solid fraction",          value = "frac_S"),
+                                                            (label = "Melt fraction",           value = "frac_M"),                                                            
                                                             (label = "Vp",                      value = "Vp"),
                                                             (label = "Vs",                      value = "Vs"),
+                                                            (label = "Bulk residual (norm)",    value = "bulk_res_norm"),
+                                                            (label = "Computation time (ms)",   value = "time_ms"),
                                                             (label = "Status",                  value = "status"),
 
                                                         ],
@@ -37,7 +45,7 @@ function Tab_PhaseDiagram()
                                 dbc_row([
                                         dbc_col([ 
                                             html_h1("Colormap", style = Dict("textAlign" => "center","font-size" => "120%", "marginTop" => 8)),
-                                        ]),
+                                        ], width=3),
                                         dbc_col([
                                             dcc_dropdown(   id          = "colormaps_cross",
                                                             options     = [String.(keys(colormaps))...],
