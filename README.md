ddb5335c1abb61bfe7d8c9dc8c42395b376fb2ee
NicolasRiel <a.nicolas.riel@gmail.com>
2024-02-02 10:10:52 +0100

    adding MAGEMin_app v0.1.0

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # MAGEMin_app.jl
 
-`MAGEMin_app.jl` provides a web-based graphical user interface for `MAGEMin`, with which you can create phase diagrams.
+`MAGEMin_app.jl` provides an easy-to-use web-based graphical user interface for `MAGEMin`. Available features:
+
+1. Compute Pressure-Temperature iso-chemical phase diagrams (PT phase diagrams)
+2. Compute Pressure or Temperature versus variable composition diagrams (PX, TX phase diagrams)
+3. Display iso-contour of phase fractions, densities, seismic velocities etc.
+4. Automatic labeling of the phase fields including listing the stable phase assemblage when the field is too small.
+5. Compute Pressure-Temperature path diagrams for fractional melting and crystallization.
+6. Save generated diagrams/figures as vector files (svg format).
+7. Export single point and whole grid information as table (ascii format)
+
+As for `MAGEMin`, you can choose among several thermodynamic dataset: Metapelite (White et al., 2014), Metabasite (Green et al., 2016), Igneous (Holland et al., 2018) or Ultramafic (Evans & Frost, 2021).
 
 > [!CAUTION]
-> As of now, this remains under development and has not yet been officially released. We recommend to not use it for publications yet. The precompiled binaries will work on various systems, but details will only be given in an upcoming publication, after which we will make an official release. 
+> As of now, `MAGEMin_app.jl` remains under development and the current release is a beta version. Reporting issues, potential improvement and contributions are most welcome.
 
 ### Installation
 
-To install this, please install the local versions of `MAGEMin_jll` and `MAGEMin_C` first, after which you can install the app itself
+To install this, please install the local versions of `MAGEMin_jll` and `MAGEMin_C` first, after which you can install the App itself
 ```julia
 julia>]
-pkg> add https://github.com/boriskaus/MAGEMin_jll.jl
-pkg> add https://github.com/boriskaus/MAGEMin_C.jl
-pkg> add https://github.com/ComputationalThermodynamics/MAGEMin_app
+pkg> add MAGEMin_app
 ```
 
 ### Running
@@ -23,21 +31,6 @@ julia>using MAGEMin_app
 julia>App()
 [ Info: Listening on: 127.0.0.1:8050, thread id: 2
 ```
-Next you can open [127.0.0.1:8050](127.0.0.1:8050) in your favorite browser.
+Next you can open [127.0.0.1:8050](127.0.0.1:8050) in your favorite browser, which will launch the App.
 
 
-
-### Development
-
-In case your first name is Nico and you want to use a locally developed library, you need to checkout a local development version of `MAGEMin_C`
-```julia
-julia>]
-pkg> rm MAGEMin_C
-pkg> dev https://github.com/boriskaus/MAGEMin_C.jl
-```
-You'll need to compile a new dynamic library for your system within the `TC_calibration` folder, place yourself in `~/.julia/dev/MAGEMin_C` and copy the following files over: 
-```
-cp -r ~/TC_calibration/gen .
-cp -r ~/TC_calibration/test .
-cp -r ~/TC_calibration/julia .
-```
--- src/appData.jl
+++ src/appData.jl
@@ -1,11 +1,6 @@
 global AppData
 
-MAGEMin_version     = "v1.3.6";
-vertice_list        = [];
-mesh                = [];
-field               = [];
 PseudosectionData   = [];
-LayoutPS            = [];
 
 
 # LIST AVAILABLE DATABASE
@@ -20,20 +15,8 @@ push!(dba,Dict(         :database    => "Metabasite (Green et al., 2016)",
                         :acronym     => "mb",
                         ), cols=:union)
 
-push!(dba,Dict(         :database    => "Igneous HP18 (Green et al., 2023)",
+push!(dba,Dict(         :database    => "Igneous (Holland et al., 2018)",
                         :acronym     => "ig",
-                        ), cols=:union)
-
-push!(dba,Dict(         :database    => "Igneous HP18 (Green et al., 2023), extended",
-                        :acronym     => "ige",
-                        ), cols=:union)
-
-push!(dba,Dict(         :database    => "Igneous T21 (Green et al., 2023)",
-                        :acronym     => "igd",
-                        ), cols=:union)
-
-push!(dba,Dict(         :database    => "Alkaline (Weller et al., 2023)",
-                        :acronym     => "alk",
                         ), cols=:union)
 
 push!(dba,Dict(         :database    => "Ultramafic (Evans & Frost., 2021)",
@@ -308,154 +291,6 @@ push!(db,Dict(          :bulk       => "predefined",
                         :frac       => [50.0810,  8.6901,  11.6698, 12.1438, 7.7832,  0.2150,  2.4978,  1.0059,  0.4670,  0.0100, 5.4364],
                         :frac2      => [],
                         ), cols=:union)
-             
-            
-#IGNEOUS EXTENDED DATABASE
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "KLB1 Peridotite - Anhydrous",
-                        :comments   => "Holland et al., 2018",
-                        :db         => "ige",
-                        :test       => 0,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [38.494,  1.776,  2.824, 50.566, 5.886,  0.01,  0.250,  0.10,  0.096,  0.109, 0],
-                        :frac2      => [],
-                        ), cols=:union)
-
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "RE46 - Icelandic basalt",
-                        :comments   => "Yang et al., 1996",
-                        :db         => "ige",
-                        :test       => 1,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [ 50.72,	9.16,15.21, 16.25,	7.06, 0.01, 1.47, 0.39, 0.35,  0.01,  0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "N_MORB - Basalt",
-                        :comments   => "Gale et al., 2013",
-                        :db         => "ige",
-                        :test       => 2,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [53.21,	9.41,	12.21,	12.21,	8.65,	0.09,	2.90,1.21,0.69,0.02, 0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-             
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "MIX1-G - Pyroxenite",
-                        :comments   => "Hirschmann et al., 2003",
-                        :db         => "ige",
-                        :test       => 3,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [45.25,	8.89,	12.22,	24.68,6.45,	0.03,	1.39,0.67,0.11,0.02,0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-             
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "High-Al basalt",
-                        :comments   => "Baker, 1983",
-                        :db         => "ige",
-                        :test       => 4,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [54.40,	12.96, 11.31, 7.68, 8.63,	0.54, 3.93, 0.79, 0.41, 0.01, 0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-             
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "Tonalite 101",
-                        :comments   => "Piwinskii, 1968",
-                        :db         => "ige",
-                        :test       => 5,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [66.01,11.98,7.06,4.16,5.30,1.57,4.12,0.66,0.97,0.01, 50],
-                        :frac2      => [],
-                        ), cols=:union)
-             
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "Wet Basalt",
-                        :comments   => "unpublished",
-                        :db         => "ige",
-                        :test       => 6,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [50.0810,  8.6901,  11.6698, 12.1438, 7.7832,  0.2150,  2.4978,  1.0059,  0.4670,  0.0100, 5.4364],
-                        :frac2      => [],
-                        ), cols=:union)
-       
-
-
-#ALKALINE DATABASE
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "Ne-syenite",
-                        :comments   => "Weller et al., 2023",
-                        :db         => "alk",
-                        :test       => 0,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [63.84, 13.72, 3.09, 1.55, 5.07, 4.04, 9.38, 0.78, 1.47, 0.01, 0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-             
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "Syenite",
-                        :comments   => "Weller et al., 2023",
-                        :db         => "alk",
-                        :test       => 1,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [70.06, 11.63, 2.76, 1.5, 4.3, 3.72, 6.41, 0.51, 0.89, 0.01, 0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "Ijolite",
-                        :comments   => "Weller et al., 2023",
-                        :db         => "alk",
-                        :test       => 2,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [48.97, 12.76, 12.87, 5.21, 7.97, 1.66, 10.66, 1.36, 1.66, 0.01, 0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-    
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "9418",
-                        :comments   => "Weller et al., 2023",
-                        :db         => "alk",
-                        :test       => 3,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [53.221, 11.671, 10.009, 6.597, 7.053, 5.582, 2.956, 0.825, 1.94, 0.146, 0.0],
-                        :frac2      => [],
-                        ), cols=:union)
-
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "KLB1 Peridotite - Anhydrous",
-                        :comments   => "Holland et al., 2018",
-                        :db         => "alk",
-                        :test       => 4,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [38.494,  1.776,  2.824, 50.566, 5.886,  0.01,  0.250,  0.10,  0.096,  0.109, 0],
-                        :frac2      => [],
-                        ), cols=:union)
-                           
-push!(db,Dict(          :bulk       => "predefined",
-                        :title      => "Wet Ijolite",
-                        :comments   => "Weller et al., 2023",
-                        :db         => "alk",
-                        :test       => 5,
-                        :sysUnit    => "mol",
-                        :oxide      => ["SiO2","Al2O3","CaO","MgO","FeO","K2O","Na2O","TiO2","O","Cr2O3","H2O"],
-                        :frac       => [48.97, 12.76, 12.87, 5.21, 7.97, 1.66, 10.66, 1.36, 1.66, 0.01, 20.0],
-                        :frac2      => [],
-                        ), cols=:union)
 
 #ULTRAMAFIC DATABASE
 push!(db,Dict(          :bulk       => "predefined",
@@ -481,25 +316,8 @@ push!(db,Dict(          :bulk       => "predefined",
                         ), cols=:union)
     
 
-layoutPS = Layout(
-            title = attr(
-                x       = 0.5,
-                xanchor = "center",
-                yanchor = "top"
-            ),
-            plot_bgcolor  = "#FFF",
-            paper_bgcolor = "#FFF",
-            width       = 800,
-            height      = 800
-        )
 
-
-AppData = ( MAGEMin_version     = MAGEMin_version,
-            vertice_list        = vertice_list,
-            mesh                = mesh,
-            field               = field,
-            db                  = db,
+AppData = ( db                  = db,
             dba                 = dba,
-            PseudosectionData   = PseudosectionData,
-            layoutPS            = layoutPS )
+            PseudosectionData   = PseudosectionData )
             
