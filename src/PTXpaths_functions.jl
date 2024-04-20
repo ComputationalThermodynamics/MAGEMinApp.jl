90a1c19fc5ee7eb65db9b2269740680cdc5bf158
NicolasRiel <a.nicolas.riel@gmail.com>
2024-04-20 17:28:44 +0200

    completed find liquidus option

--- src/PTXpaths_functions.jl
+++ src/PTXpaths_functions.jl
@@ -183,10 +183,9 @@ function compute_Tliq(          pressure,   bulk_ini,   oxi,
                                 verbose,    bulk,       bufferN,
                                 cpx,        limOpx,     limOpxVal       )
 
-
-        Tliq = "oki"
-
-
+        Tsol        = 600.0;
+        Tliq        = 2200.0;
+                        
         out = MAGEMin_C.gmin_struct{Float64, Int64}
 
         mbCpx,limitCaOpx,CaOpxLim,sol = get_init_param( dtb,        solver,
@@ -204,64 +203,58 @@ function compute_Tliq(          pressure,   bulk_ini,   oxi,
         sys_in  = "mol"
         gv      =  define_bulk_rock(gv, bulk_ini, oxi, sys_in, dtb);
 
-        out = deepcopy( point_wise_minimization(pressure,2000.0, gv, z_b, DB, splx_data, sys_in) )
+        out     = deepcopy( point_wise_minimization(pressure,Tliq, gv, z_b, DB, splx_data, sys_in) )
+        ref     = out.ph
+        nph     = length(out.ph)
+        if (nph > 1)
+            print("Warning at $Tliq °C, one or several solid phases are stable: $(out.ph)\n")
+            print(" - This likely means that one of the oxide of the database $dtb does not enter the melt chemical space!\n")
+            print(" - The current assemblage is therefore taken as a reference for supra-liquidus conditions\n")
+        end
 
-        print("$out\n")
+
+        tol         = 1e-2
+        n_max       = 32
+
+        a           = Tsol
+        b           = Tliq
+        n           = 1
+        conv        = 0
+        n           = 0
+        sign_a      = -1
+
+
+        while n < n_max && conv == 0
+            c = (a+b)/2.0
+
+            out     = deepcopy( point_wise_minimization(pressure, c , gv, z_b, DB, splx_data, sys_in) )
+            cmp     = setdiff(out.ph,ref)
+
+            if isempty(cmp)
+                result = 1;
+            else
+                result = -1;
+            end
+
+            sign_c  = sign(result)
+
+            if abs(b-a) < tol
+                conv = 1
+            else
+                if  sign_c == sign_a
+                    a = c
+                    sign_a = sign_c
+                else
+                    b = c
+                end
+                
+            end
+            n += 1
+        end
 
         LibMAGEMin.FreeDatabases(gv, DB, z_b)
 
-
-
-
-    # # allocate
-    # n_xeos      = SS_ref_db[ph_id].n_xeos
-    # n_pc        = SS_ref_db[ph_id].n_em
-
-    # pc_xeos     = zeros(n_pc,n_xeos)
-    # pc_comp     = zeros(n_pc,gv.len_ox)
-    # pc_G0       = zeros(n_pc)
-
-    # tol         = 1e-6
-    # target_dg   = 1e-4
-    # n_max       = 32
-
-    # for i=1:n_pc
-    #     bump        = (rand(SS_ref_db[ph_id].n_xeos).-0.5)./100
-    #     delta_g     = 1.0                                                   # initialize missfit
-    #     a           = 0.0
-    #     b           = 1.0
-    #     n           = 1
-    #     conv        = 0
-    #     n           = 0
-    #     sign_a      = -1
-
-
-    #     while n < n_max && conv == 0
-    #         c = (a+b)/2.0
-    #         delta_g, comp_norm, g0 = get_delta_g(mSS_vec,id,ph,ph_id,bump,c,SS_ref_db,gv,z_b,target_dg)
-    #         sign_c  = sign(delta_g)
-    #         # print("delta_g: $delta_g\n")
-    #         if abs(delta_g) < tol
-    #             conv = 1
-    #             pc_xeos[i,:] .= mSS_vec[id].xeos_Ppc .+ bump.*c
-    #             pc_comp[i,:] .= comp_norm
-    #             pc_G0[i]      = g0
-    #         else
-    #             if  sign_c == sign_a
-    #                 a = c
-    #                 sign_a = sign_c
-    #             else
-    #                 b = c
-    #             end
-                
-    #         end
-    #         n += 1
-    #     end
-
-    # end
-
-
-
+        Tliq  = string((a+b)/2.0)
         return Tliq
 end
 
