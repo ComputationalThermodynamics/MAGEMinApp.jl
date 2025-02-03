3c0945bb7de57223abe860c71246bd00ea8c9f00
Nicolas Riel <a.nicolas.riel@gmail.com>
2025-02-03 14:44:32 +0100

    added IW buffer to the list

--- src/Tab_PTXpaths.jl
+++ src/Tab_PTXpaths.jl
@@ -203,6 +203,7 @@ function Tab_PTXpaths(db_inf)
                                                 (label = "no buffer",value = "none"),
                                                 (label = "QFM",      value = "qfm"),
                                                 (label = "MW",       value = "mw"), 
+                                                (label = "IW",       value = "iw"), 
                                                 (label = "QIF",      value = "qif"),
                                                 (label = "CCO",      value = "cco"),
                                                 (label = "HM",       value = "hm"), 
